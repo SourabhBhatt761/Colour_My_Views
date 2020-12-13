eb0ecd5daf3ce4fc1bf4be5b9eb4ea65ed530b47
SourabhBhatt761 <bhattsourabh20@gmail.com>
2020-12-13 15:55:49 +0530

    Initial commit

---
+++ app/src/main/java/com/srb/colourmyviews/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.srb.colourmyviews
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setListeners()
+    }
+
+    private fun setListeners() {
+
+        val clickableViews: List<View> =
+            listOf(box_one, box_two, box_three, box_four, box_five, button_green, button_red, button_yellow, text_view)
+
+        for (item in clickableViews) {
+            item.setOnClickListener { makeColoured(it) }
+
+        }
+    }
+
+    private fun makeColoured(view: View) {
+
+        when(view.id){
+
+            // Boxes using Color class colors for background
+            R.id.box_one -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two -> view.setBackgroundColor(Color.GRAY)
+
+            // Boxes using Android color resources for background
+            R.id.box_three -> view.setBackgroundResource(android.R.color.holo_green_light)
+            R.id.box_four -> view.setBackgroundResource(android.R.color.holo_green_dark)
+            R.id.box_five -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            // Boxes using custom colors for background
+            R.id.button_red -> box_three.setBackgroundResource(R.color.my_red)
+            R.id.button_yellow -> box_four.setBackgroundResource(R.color.my_yellow)
+            R.id.button_green -> box_five.setBackgroundResource(R.color.my_green)
+
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
+    }
+}
